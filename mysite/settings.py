bd6fd50469ac5d6e07c57d66514010514f7bb801
seh5 <seh5@njit.edu>
2021-12-02 12:20:42 -0500

    i think i did it!

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'rest_framework'
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
